d79854e975c07dcc3dc8ae9350c40f7b8788eca9
Stepan Horenek <stepan.horenek@gmail.com>
2026-03-08 20:39:53 +0100

    Added dynamic allocation (bit_array_alloc) and freeing of dynamic bit_array (bit_array_free)

--- bitarray.h
+++ bitarray.h
@@ -1,6 +1,7 @@
 #ifndef BIT_ARRAY
 #define BIT_ARRAY 1
 #include <assert.h>
+#include <stdlib.h>
 
 typedef unsigned long* bit_array;
 typedef unsigned long bit_array_index_t;
@@ -10,4 +11,14 @@ typedef unsigned long bit_array_index_t;
     _Static_assert( size <= ULONG_MAX && size > 0, "Error: Invalid size of bitarray during static creation"); \
     bit_array name[1 + ((size + UL_SIZE - 1)/ UL_SIZE)] = {(usigned long)(size), 0};
 
+#define bit_array_alloc(name, size) \
+    assert( size <= ULONG_MAX && size > 0); \
+    bit_array name = calloc(1 + ((size + UL_SIZE - 1)/ UL_SIZE), sizeof(unsigned long)); \
+    if (name == NULL){ \
+        fprintf(stderr, "bitarray_alloc: Chyba alokace paměti"); \
+        exit(1); \
+    } \
+    name[0] = (unsigned long)(size); \
+
+#define bit_array_free(name) (free(name))
 #endif

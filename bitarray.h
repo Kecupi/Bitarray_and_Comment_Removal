810a2cbd4aaa4c9c98b63c31d5a17be7d1416d0c
Stepan Horenek <stepan.horenek@gmail.com>
2026-03-09 19:33:57 +0100

    Renamed functions to fit the project requirements, fixed more typos, included stdio.h

--- bitarray.h
+++ bitarray.h
@@ -1,71 +1,75 @@
-#ifndef BIT_ARRAY
-#define BIT_ARRAY 1
+#ifndef BITARRAY_H
+#define BITARRAY_H 1
+
 #include <assert.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-typedef unsigned long* bit_array;
-typedef unsigned long bit_array_index_t;
+typedef unsigned long* bitarray_t;
+typedef unsigned long bitarray_index_t;
 #define UL_SIZE (sizeof(unsigned long) * CHAR_BIT)
 
-#define bit_array_create(name, size) \
+#define bitarray_create(name, size) \
     static_assert(size <= ULONG_MAX && size > 0, "Error: Invalid size of bitarray during static creation"); \
-    bit_array name[1 + ((size + UL_SIZE - 1)/ UL_SIZE)] = {(unsigned long)(size), 0}
+    bitarray_index_t name[1 + ((size + UL_SIZE - 1)/ UL_SIZE)] = {size, 0}
 
-#define bit_array_alloc(name, size) \
+#define bitarray_alloc(name, size) \
     assert(size <= ULONG_MAX && size > 0); \
-    bit_array name = calloc(1 + ((size + UL_SIZE - 1)/ UL_SIZE), sizeof(unsigned long)); \
+    bitarray_t name = calloc(1 + ((size + UL_SIZE - 1)/ UL_SIZE), sizeof(bitarray_index_t)); \
     if (name == NULL){ \
         fprintf(stderr, "bitarray_alloc: Chyba alokace paměti"); \
         exit(1); \
     } \
-    name[0] = (unsigned long)(size)
+    name[0] = size
 
 #ifndef USE_INLINE
 
-#define bit_array_free(name) (free(name))
+#define bitarray_free(name) \
+    (free(name))
 
-#define bit_array_size(name) (name[0])
+#define bitarray_size(name) \
+    (name[0])
 
-#define bit_array_fill(name, bool_in) \
+#define bitarray_fill(name, bool_in) \
     do { \
-        unsigned long idx_count = 1 + ((bit_array_size(name)) + UL_SIZE - 1)/ UL_SIZE; \
-        for (unsigned long idx = 1; idx < idx_count ; idx++){ \
+        bitarray_index_t idx_count = 1 + ((bitarray_size(name)) + UL_SIZE - 1)/ UL_SIZE; \
+        for (bitarray_index_t idx = 1; idx < idx_count ; idx++){ \
             name[idx] = bool_in ? ULONG_MAX : 0; \
         } \
     } while (0)
 
-#define bit_array_setbit(name, index, bool_in) \
-    bool_in ? name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE)): name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE)); \
+#define bitarray_setbit(name, index, bool_in) \
+    bool_in ? name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE)) : name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE))
 
-#define bit_array_getbit(name, index) \
+#define bitarray_getbit(name, index) \
     ((name[1 + (index / UL_SIZE)] >> (index % UL_SIZE)) & 1UL)
 
 #else
 
-inline void bit_array_free(bit_array name){
+inline void bitarray_free(bitarray_t name){
     free(name);
 }
 
-inline unsigned long bit_array_size(bit_array name){
+inline bitarray_index_t bitarray_size(bitarray_t name){
     return name[0];
 }
 
-inline void bit_array_fill(bit_array name, bool bool_in){
-    unsigned long idx_count = 1 + ((bit_array_size(name)) + UL_SIZE - 1)/ UL_SIZE;
-    for (unsigned long idx = 1; idx < idx_count ; idx++){
+inline void bitarray_fill(bitarray_t name, bool bool_in){
+    bitarray_index_t idx_count = 1 + ((bitarray_size(name)) + UL_SIZE - 1)/ UL_SIZE;
+    for (bitarray_index_t idx = 1; idx < idx_count ; idx++){
         name[idx] = bool_in ? ULONG_MAX : 0;
     }
 }
 
-inline void bit_array_setbit(bit_array name, unsigned long index, bool bool_in){
+inline void bitarray_setbit(bitarray_t name, bitarray_index_t index, bool bool_in){
     bool_in ? name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE)): name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE));
 }
 
-inline unsigned long bit_array_getbit(bit_array name, unsigned long index){
+inline bitarray_index_t bitarray_getbit(bitarray_t name, bitarray_index_t index){
     return ((name[1 + (index / UL_SIZE)] >> (index % UL_SIZE)) & 1UL);
 }
 
-#endif
+#endif // end of USE_INLINE condition
 #endif

f55a14653edcc7b12cc1b5f073262a2e6818cc5c
Stepan Horenek <stepan.horenek@gmail.com>
2026-03-11 23:15:00 +0100

    Added no-comment.c and implemented basic removal of /**/ comments in C via state automaton

---
+++ no-comment.c
@@ -0,0 +1,75 @@
+#include "error.h"
+
+int main(int argc, char** argv){
+    FILE* input = stdin;
+    if (argc == 2){
+        input = fopen(argv[1], "r");
+        if (input == NULL){
+            error_exit("Couldn't open file");
+        }
+    }
+    int c;
+    int state = 0;
+    while ((c = getchar(input)) != EOF){
+        switch(state){
+            case 0:
+                if (c == "/"){
+                    state = 1;
+                } else if (c == '"'){
+                    state = 4;
+                    putchar(c);
+                } else {
+                    putchar(c)
+                }
+                break;
+            case 1:
+                if (c == "/"){
+                    putchar(c);
+                } else if (c == "*"){
+                    state = 2;
+                } else {
+                    putchar("/");
+                    putchar(c);
+                    state = 0;
+                }
+                break;
+            case 2:
+                if (c == "*"){
+                    state = 3;
+                }
+                break;
+            case 3:
+                if (c == "/"){
+                    putchar(" ");
+                    state = 0;
+                } else if (c != "*"){
+                    state = 2;
+                }
+                break;
+            case 4:
+                if (c == '"'){
+                    putchar(c);
+                    state = 0;
+                } else if (c == '\\'){
+                    state = 5;
+                    putchar(c);
+                } else {
+                    putchar(c);
+                }
+                break;
+            case 5:
+                putchar(c);
+                state = 4;
+                break;
+            default:
+                break;
+        }
+    }
+    putchar("\n");
+    if (state != 0){
+        error_exit("Unclosed comment")
+    }
+    if (argc == 2){
+        fclose(input);
+    }
+}

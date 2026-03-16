f2862ea3425d4294634778a101ec7c9d854e8b7a
Stepan Horenek <stepan.horenek@gmail.com>
2026-03-16 18:09:04 +0100

    Included error.h in bitarray.h, added bounday checks in bitarray_getbit and bitarray_setbit, added .PHONY in Makefile

--- Makefile
+++ Makefile
@@ -2,15 +2,17 @@ CC=gcc
 CFLAGS=-g -std=c11 -pedantic -Wall -Wextra -O2
 LDLIBS=-lm
 
+.PHONY: all run zip clean
+
 all: primes primes-i no-comment
 
-primes:
+primes: primes.c eratosthenes.c bitarray.c error.c
 	$(CC) $(CFLAGS) primes.c eratosthenes.c bitarray.c error.c -o $@ $(LDLIBS)
 
-primes-i:
+primes-i: primes.c eratosthenes.c bitarray.c error.c
 	$(CC) $(CFLAGS) -DUSE_INLINE primes.c eratosthenes.c bitarray.c error.c -o $@ $(LDLIBS)
 
-no-comment:
+no-comment: no-comment.c error.c
 	$(CC) $(CFLAGS) no-comment.c error.c -o no-comment
 
 run: primes primes-i
--- bitarray.h
+++ bitarray.h
@@ -6,21 +6,21 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "error.h"
 
 typedef unsigned long* bitarray_t;
 typedef unsigned long bitarray_index_t;
 #define UL_SIZE (sizeof(unsigned long) * CHAR_BIT)
 
 #define bitarray_create(name, size) \
-    static_assert(size <= ULONG_MAX && size > 0, "Error: Invalid size of bitarray during static creation"); \
+    static_assert(size <= ULONG_MAX && size > 0, "bitarray_create: Chybná velikost seznamu\n"); \
     bitarray_index_t name[1 + ((size + UL_SIZE - 1)/ UL_SIZE)] = {size, 0}
 
 #define bitarray_alloc(name, size) \
     assert(size <= ULONG_MAX && size > 0); \
     bitarray_t name = calloc(1 + ((size + UL_SIZE - 1)/ UL_SIZE), sizeof(bitarray_index_t)); \
     if (name == NULL){ \
-        fprintf(stderr, "bitarray_alloc: Chyba alokace paměti"); \
-        exit(1); \
+        error_exit("bitarray_alloc: Chyba alokace paměti"); \
     } \
     name[0] = size
 
@@ -40,11 +40,29 @@ typedef unsigned long bitarray_index_t;
         } \
     } while (0)
 
+#ifdef NO_CHECK
+
 #define bitarray_setbit(name, index, bool_in) \
     bool_in ? (name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE))) : (name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE)))
 
 #define bitarray_getbit(name, index) \
     ((name[1 + (index / UL_SIZE)] >> (index % UL_SIZE)) & 1UL)
+
+#else
+
+#define bitarray_setbit(name, index, bool_in) \
+    (index >= bitarray_size(name)) ? \
+    (error_exit("bitarray_setbit: Index %lu mimo rozsah 0..%lu", (unsigned long)index, (unsigned long)(bitarray_size(name)-1)), 0) : \
+    (bool_in ? \
+    (name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE))) : \
+    (name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE))))
+
+#define bitarray_getbit(name, index) \
+    (index >= bitarray_size(name)) ? \
+    (error_exit("bitarray_getbit: Index %lu mimo rozsah 0..%lu", (unsigned long)index, (unsigned long)(bitarray_size(name)-1)), 0) : \
+    ((name[1 + (index / UL_SIZE)] >> (index % UL_SIZE)) & 1UL)
+
+#endif // end of NO_CHECK
 
 #else
 
@@ -64,10 +82,20 @@ inline void bitarray_fill(bitarray_t name, bool bool_in){
 }
 
 inline void bitarray_setbit(bitarray_t name, bitarray_index_t index, bool bool_in){
+    #ifndef NO_CHECK
+    if (index >= name[0]){
+        error_exit("bitarray_setbit: Index %lu mimo rozsah 0..%lu", (unsigned long)index, (unsigned long)(bitarray_size(name)-1));
+    }
+    #endif
     bool_in ? (name[1 + (index / UL_SIZE)] |= (1UL << (index % UL_SIZE))) : (name[1 + (index / UL_SIZE)] &= ~(1UL << (index % UL_SIZE)));
 }
 
 inline bool bitarray_getbit(bitarray_t name, bitarray_index_t index){
+    #ifndef NO_CHECK
+    if (index >= bitarray_size(name)){
+        error_exit("bitarray_getbit: Index %lu mimo rozsah 0..%lu", (unsigned long)index, (unsigned long)(bitarray_size(name)-1));
+    }
+    #endif
     return ((name[1 + (index / UL_SIZE)] >> (index % UL_SIZE)) & 1UL);
 }
 
